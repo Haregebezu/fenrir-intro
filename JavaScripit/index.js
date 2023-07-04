86d2733916f96e8d110e30e87619ce4d7edec05a
Hareg <Haregebezuamare@6588@gmail.com>
2023-07-04 16:49:53 -0500

    lesson-4-5

--- JavaScripit/index.js
+++ JavaScripit/index.js
@@ -4,15 +4,6 @@ var footer = document.querySelector("footer");
 var copyright = document.createElement('p');
 footer.appendChild(copyright);
 copyright.innerHTML = "\u00A9 " + thisYear;
-var skills = [
-    "HTML",
-    "CSS",
-    "JavaScript",
-    "Version control(Git)",
-    "Browser Developer Tools",
-    "Debugging and Troubleshooting",
-    "Responsive web Design"
-];
 var skillsSection = document.querySelector("#skills");
 var skillsList = skillsSection.querySelector('ul');
 for (var i = 0; i < skills.length; i++) {
